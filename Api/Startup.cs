69d6cb2ab0fbf7a4ee373e21b6adef7dd35d244e
Matt Jones <github@jonesm.io>
2019-04-15 05:47:48 +0100

    add SimpleInjector and integrate

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -3,11 +3,18 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Controllers;
+    using Microsoft.AspNetCore.Mvc.ViewComponents;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using SimpleInjector;
+    using SimpleInjector.Integration.AspNetCore.Mvc;
+    using SimpleInjector.Lifestyles;
 
     public class Startup
     {
+        Container container = new Container();
+    
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -17,12 +24,42 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
+            services
+                .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
+            IntegrateSimpleInjector(services);
         }
 
+        void IntegrateSimpleInjector(IServiceCollection services)
+        {
+            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+
+            services.AddHttpContextAccessor();
+
+            services.AddSingleton<IControllerActivator>(
+                new SimpleInjectorControllerActivator(container));
+            
+            services.AddSingleton<IViewComponentActivator>(
+                new SimpleInjectorViewComponentActivator(container));
+
+            services.EnableSimpleInjectorCrossWiring(container);
+            services.UseSimpleInjectorAspNetRequestScoping(container);
+        }
+        
+        void InitializeContainer(IApplicationBuilder app)
+        {
+            container.RegisterMvcControllers(app);
+            container.RegisterMvcViewComponents(app);
+
+            container.AutoCrossWireAspNetComponents(app);
+        }
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeContainer(app);
+            container.Verify();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
